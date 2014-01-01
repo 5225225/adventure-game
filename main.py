6c680ac685539d8c2b5423710a0dc8691873c137
5225225 <mrhouse991@gmail.com>
2014-01-01 10:52:35 +0000

    Fixed all pep8 errors

--- main.py
+++ main.py
@@ -3,22 +3,22 @@ import json
 
 from messages import getmsg
 
-rooms = []
-inventory = []
 
 class room():
-    def __init__(self,pos,items,things,name):
+    def __init__(self, pos, items, things, name):
         self.pos = pos
         self.items = items
         self.things = things
         self.name = name
 
-def search(searchlist,item):
-    for index,i in enumerate(searchlist):
+
+def search(searchlist, item):
+    for index, i in enumerate(searchlist):
         if item == i:
             return index
 
-def do_action(action,thing,ITEM):
+
+def do_action(action, thing, ITEM):
     #The thing and item are needed to be able to rerun functions using MATCH
     #Do_action is only done a few times in the code, so this shouldn't be too
     #bad, but a better way would be preferable
@@ -28,67 +28,77 @@ def do_action(action,thing,ITEM):
         if act[0] == "TAKE":
             try:
                 inventory.remove(" ".join(act[1:]))
-                print(getmsg("TAKE-ACT").format(thing["name"]," ".join(act[1:])))
+                print(getmsg("TAKE-ACT").format(
+                    thing["name"], " ".join(act[1:])))
             except ValueError:
-                print(getmsg("TAKE-ACT-FAILED").format(thing["name"]," ".join(act[1:])))
+                print(getmsg("TAKE-ACT-FAILED").format(
+                    thing["name"], " ".join(act[1:])))
         elif act[0] == "GIVE":
             inventory.append(" ".join(act[1:]))
-            print(getmsg("GIVE-ACT").format(thing["name"]," ".join(act[1:])))
+            print(getmsg("GIVE-ACT").format(thing["name"], " ".join(act[1:])))
         elif act[0] == "TELEPORT":
             global playerpos
-            playerpos = tuple(map(int,act[1].split(",")))
+            playerpos = tuple(map(int, act[1].split(",")))
             print(getmsg("TELE-ACT").format(thing["name"]))
         elif act[0] == "MATCH":
             prompt = " ".join(act[1:]).split(",")[0]
             value = " ".join(act[1:]).split(",")[1]
             instr = input(prompt)
+            actions = thing["actions"]
             if instr == value:
-                do_action(thing["actions"]["_{}_yes".format(ITEM)],thing,ITEM)
+                do_action(actions["_{}_yes".format(ITEM)], thing, ITEM)
             else:
-                do_action(thing["actions"]["_{}_no".format(ITEM)],thing,ITEM)
-        elif act[0] == "COMP": 
+                do_action(actions["_{}_no".format(ITEM)], thing, ITEM)
+        elif act[0] == "COMP":
             prompt = " ".join(act[1:]).split(",")[0]
             value = " ".join(act[1:]).split(",")[1]
             instr = input(prompt)
             try:
+                actions = thing["actions"]
                 if int(instr) > int(value):
-                    do_action(thing["actions"]["_{}_high".format(ITEM)],thing,ITEM)
+                    do_action(actions["_{}_high".format(ITEM)], thing, ITEM)
                 elif int(instr) < int(value):
-                    do_action(thing["actions"]["_{}_low".format(ITEM)],thing,ITEM)
+                    do_action(actions["_{}_low".format(ITEM)], thing, ITEM)
                 elif int(instr) == int(value):
-                    do_action(thing["actions"]["_{}_equal".format(ITEM)],thing,ITEM)
+                    do_action(actions["_{}_equal".format(ITEM)], thing, ITEM)
                 else:
                     #This REALLY should never happen
                     assert False
             except ValueError:
                 print(getmsg("NAN"))
         elif act[0] == "SAY":
-            print(" ".join(act[1:])) 
-        
+            print(" ".join(act[1:]))
+
+
 def findroom(pos):
     for item in rooms:
         if item.pos == list(pos):
             return item
 
+
 def roomexists(pos):
-    if findroom(pos) == None:
+    if findroom(pos) is None:
         return False
     else:
         return True
-def moveplayer(ppos,pos): #Movement is reletive to current position
+
+
+def moveplayer(ppos, pos):  # Movement is reletive to current position
     new = (ppos[0] + pos[0], ppos[1] + pos[1])
     if roomexists(new):
         global playerpos
         playerpos = new
-        return True #Not an ideal way of returning a status, but it'll do.
+        return True  # Not an ideal way of returning a status, but it'll do.
     else:
         return False
 
-playerpos = (0,0)
+playerpos = (0, 0)
+rooms = []
+inventory = []
 
 with open("rooms.dat") as roomfile:
     for r in json.loads(roomfile.read()):
-        rooms.append(room(r["pos"],r["items"],r["things"],r["name"]))
+        rooms.append(room(r["pos"], r["items"], r["things"], r["name"]))
 
 try:
     with open("save.json") as savefile:
@@ -102,17 +112,21 @@ except FileNotFoundError:
 while True:
     instr = input(": ").split(" ")
     #Oh joy, text parsing code
-    if instr == [""]: pass
+    if instr == [""]:
+        pass
     #Moving player
-    elif instr[0] in ("move","go"):
-
+    elif instr[0] in ("move", "go"):
         try:
-            if instr[1] in ("north","up"): direction = [0,1] 
-            if instr[1] in ("east","right"): direction = [1,0]
-            if instr[1] in ("south","down"): direction = [0,-1]
-            if instr[1] in ("west","left"): direction = [-1,0]
+            if instr[1] in ("north", "up"):
+                direction = [0, 1]
+            if instr[1] in ("east", "right"):
+                direction = [1, 0]
+            if instr[1] in ("south", "down"):
+                direction = [0, -1]
+            if instr[1] in ("west", "left"):
+                direction = [-1, 0]
 
-            if moveplayer(playerpos,direction):
+            if moveplayer(playerpos, direction):
                 print(getmsg("NEW-ROOM"))
             else:
                 print(getmsg("NO-DOOR"))
@@ -124,9 +138,9 @@ while True:
     elif instr[0] in ("take"):
         if len(instr) == 1:
             print(getmsg("NO-ITEM"))
-        else: 
+        else:
             room = findroom(playerpos)
-            if instr[1] in ("all","everything"):
+            if instr[1] in ("all", "everything"):
                 for item in room.items:
                     inventory.append(item)
                 room.items = []
@@ -138,25 +152,25 @@ while True:
                         break
                 else:
                     print(getmsg("NO-ITEM"))
-    elif instr[0] in ("inv","inventory"):
+    elif instr[0] in ("inv", "inventory"):
         for item in inventory:
             print (item)
-    
-    elif instr[0] in ("search","look"):
+
+    elif instr[0] in ("search", "look"):
         print(getmsg("LOOK-NAME").format(findroom(playerpos).name))
         print(getmsg("LOOK-ITEMS"))
         for item in findroom(playerpos).items:
-            print(" "+ item)
+            print(" " + item)
         print(getmsg("LOOK-THINGS"))
         for item in findroom(playerpos).things:
             print(" " + item["name"])
-    
+
     elif instr[0] in ("use"):
-        if " ".join(instr[1:search(instr,"on")]) in inventory:
-            ITEM = " ".join(instr[1:search(instr,"on")])
+        if " ".join(instr[1:search(instr, "on")]) in inventory:
+            ITEM = " ".join(instr[1:search(instr, "on")])
             thing = ""
             for item in findroom(playerpos).things:
-                if item["name"] == " ".join(instr[search(instr,"on") + 1:]):
+                if item["name"] == " ".join(instr[search(instr, "on") + 1:]):
                     thing = item
                     break
             if thing == "":
@@ -164,15 +178,17 @@ while True:
             else:
                 try:
                     action = thing["actions"][ITEM]
-                    do_action(action,thing,ITEM)
+                    do_action(action, thing, ITEM)
                 except KeyError:
-                    pass #This thing can't handle this item
+                    pass
+                    #This thing can't handle this item
     elif instr[0] in ("help"):
         print(getmsg("HELPTEXT"))
     elif instr[0] in ("quit"):
-        with open("save.json","w") as savefile:
-            savedat = {"pos":playerpos,"items":inventory}
-            json.dump(savedat,savefile)
+        with open("save.json", "w") as savefile:
+            savedat = {"pos": playerpos, "items": inventory}
+            json.dump(savedat, savefile)
         sys.exit(0)
     else:
         print(getmsg("INVALID-COMMAND").format(" ".join(instr)))
+        pass
--- messages.py
+++ messages.py
@@ -1,51 +1,54 @@
 #A place to store game messages without hard-coding them into the source.
 
 messages = {
-"NO-DOOR":"There doesn't seem to be a room in that direction...",
-"NEW-ROOM":"You open the door, and walk into a room.",
-"NO-ITEM":"404 item not found.",
-"NO-DIR":"But where do you want to go?",
-"INVALID-DIR":"You don't know how to go there. No one knows how to go there.",
-"NO-THING":"418 I'm a teapot, and I don't know what thing you're talking about.",
-#Works a lot better if you've seen the NO-ITEM first
-"LOOK-NAME":"This room's name is {}.",
-"LOOK-ITEMS":"You look around, and find the following items:",
-"LOOK-THINGS":"You also see the following things in the room",
-"TAKE-ACT":"The {} has taken your {}!",
-"NAN":"That isn't a number... We're using base 10.",
-"TAKE-ACT-FAILED":"The {} has tried to take a {}, but you didn't have one!",
-"GIVE-ACT":"The {} has given you a {}!",
-"TELE-ACT":"The {} has teleported you!",
-"INVALID-COMMAND":"I don't know what you mean by {}.",
-"HELPTEXT":"""
-move,go: Moves your player in the specified direction
-    format: go <up/down/left/right>
-    Directions can be given using up,down,left,right or north,south,east,west
-    If there is not a door in the direction, a message will be given.
-    If there is a door, your player will be moved, with a message.
+    "NO-DOOR": "There doesn't seem to be a room in that direction...",
+    "NEW-ROOM": "You open the door, and walk into a room.",
+    "NO-ITEM": "404 item not found.",
+    "NO-DIR": "But where do you want to go?",
+    "INVALID-DIR": "You don't know how to go there.",
+    "NO-THING": "418 I'm a teapot, I don't know what you're talking about.",
+    #Works a lot better if you've seen the NO-ITEM first
+    "LOOK-NAME": "This room's name is {}.",
+    "LOOK-ITEMS": "You look around, and find the following items:",
+    "LOOK-THINGS": "You also see the following things in the room",
+    "TAKE-ACT": "The {} has taken your {}!",
+    "NAN": "That isn't a number... We're using base 10.",
+    "TAKE-ACT-FAILED": "The {} has tried to take a {}!",
+    "GIVE-ACT": "The {} has given you a {}!",
+    "TELE-ACT": "The {} has teleported you!",
+    "INVALID-COMMAND": "I don't know what you mean by {}.",
+    "HELPTEXT": """
+    move,go: Moves your player in the specified direction
+        format: go <up/down/left/right>
+        Directions can be given using up,down,left,right,
+        or using north,south,east,west, they act the same.
+        If there is not a door in the direction, a message will be given.
+        If there is a door, your player will be moved, with a message.
 
-take: Adds any specified item to your inventory
-    format: take <item/all>
-    You can either specify an item, or use all,everything to take everything.
+    take: Adds any specified item to your inventory
+        format: take <item/all>
+        You can either specify an item, or use all to take everything.
 
-inv,inventory: Prints the contents of your inventory.
-    format: inv
-    Doesn't take any input, prints every item in your inventory, 1 per line.
+    inv,inventory: Prints the contents of your inventory.
+        format: inv
+        Doesn't take any input, prints every item that you have, 1 per line.
 
-search,look: Looks around
-    format: look
-    Prints the name of the room you are in, every item in it, and every thing.
+    search,look: Looks around
+        format: look
+        Prints the name of the room you are in, every item in it,
+        and every thing in the room. You can't look at specific items yet.
 
-use: Uses one item on a thing.
-    format: use <item> on <thing>
-    When given both the name of an item, and a thing, will attempt to use
-    the item on the thing. No message will be printed if this fails.
+    use: Uses one item on a thing.
+        format: use <item> on <thing>
+        When given both the name of an item, and a thing, will attempt to use
+        the item on the thing. No message will be printed if this fails.
 
-help: Prints the help text
-    format: help
-    I'm assuming you know how to invoke the help text.
-"""
+    help: Prints the help text
+        format: help
+        I'm assuming you know how to invoke the help text.
+    """
 }
+
 
 def getmsg(code):
     return messages[code]
